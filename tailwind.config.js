c106e39d33371d7e0c2ea82b99a2e75a47fbb0bc
aziz <saidazizz132@gmail.com>
2025-12-02 23:25:29 +0000

    aziz && salim Interface

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'colearn-teal': '#4BA5A6',
+        'colearn-dark-teal': '#3A8889',
+      },
+    },
+  },
+  plugins: [],
+}
